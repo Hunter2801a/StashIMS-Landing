6f3f879cbeb64772d6d523c332bafa971392cea4
Adrián Jiménez <adrianalbertojimenez@gmail.com>
2026-06-14 17:34:41 -0500

    nuevos ajustes para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-    site: 'https://hunter2801a.github.io',
-    base: '/StashIMS-Landing',
+    site: isGitHubPages ? 'https://hunter2801a.github.io' : 'https://stash-ims-landing.vercel.app',
+    base: isGitHubPages ? '/StashIMS-Landing' : '',
     output: 'static',
 });
